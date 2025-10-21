dd800261a007d6de0a278c6180c9dffbfcc92f53
Lima <143853021+Lima-Lua@users.noreply.github.com>
2025-10-20 21:47:30 -0300

    Create PDO.php

---
+++ PDO.php
@@ -0,0 +1,17 @@
+<?php
+$host = 'localhost';
+$dbname = 'cardapio';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Conexão bem-sucedida!";
+} catch (PDOException $e) {
+    echo "Erro na conexão: " . $e->getMessage();
+}
+return $pdo;
+
+
+?>
